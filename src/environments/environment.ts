cb2d454db93a810b3585181e8a6a31a6f79148c9
Ray Govaski <rayygovaski@gmail.com>
2025-06-05 19:47:50 -0300

    atualização admin

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     apiKey: "AIzaSyB6UgDzAzuCMs4ho3FLl0Cw6fb99FrMjbY",
     authDomain: "mentalsus-f6eb2.firebaseapp.com",
     projectId: "mentalsus-f6eb2",
-    storageBucket: "mentalsus-f6eb2.appspot.com",
+    storageBucket: "mentalsus-f6eb2.firebasestorage.app",
     messagingSenderId: "406950774155",
     appId: "1:406950774155:web:d8a642fdee08b9f56d9b8e"
   }
