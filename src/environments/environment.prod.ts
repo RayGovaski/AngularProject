4d901ebfdbf7b75adcc29035ec43404b8af50887
Ray Govaski <rayygovaski@gmail.com>
2025-06-13 23:59:03 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyB6UgDzAzuCMs4ho3FLl0Cw6fb99FrMjbY",
+    authDomain: "mental-sus.vercel.app",
+    projectId: "mentalsus-f6eb2",
+    storageBucket: "mentalsus-f6eb2.firebasestorage.app",
+    messagingSenderId: "406950774155",
+    appId: "1:406950774155:web:d8a642fdee08b9f56d9b8e"
   }
 };
